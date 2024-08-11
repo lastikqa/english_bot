d67c49d9724060e0f0743633f1c5775054cf783e
lastikqa <m.a.lestat13@gmail.com>
2024-08-11 18:24:28 +0500

    updated "help"

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -11,38 +11,10 @@ menu_keyboard = {"/translation":
 default_menu = "Menu Button"
 
 chuck_keyboard = {"Next": "/chuck", "Translate": "/translation"}
+
 abnormal_verbs_keyboard = {"Next Verb": "abnormal_verbs"}
+
 phrasal_verbs_keyboard = {"Next Verb": "phrasal_verbs"}
+
 english_idioms_keyboard = {"Next Idiom": "/idioms"}
 
-help_message = r"""**_Guess Word game_**
-
-You need to choose rights translation for words
-
-The word can be russian to english or english to russian translations
-
-
-**_Words Constructor_**
-
-You need to create a right word from its own letters
-
-Of course it is a random order of the letters
-
-
-**_Phrase Constructor_**
-
-You need to create a right sentence from its own words
-
-Of course it is a random order of the words
-
-
-_Remember the more your wins in a row the higher your rating_
-
-_If u wins 3 times in a row u get 3 as your score_
-
-_\!setnicname 'Nickname' to set your nickname_
-
-_\!setidiom 'stir up a hornet's nest: provoke trouble' to add idioms_
-
-_\!setlanguage 'de'  German language use language codes_
-"""
